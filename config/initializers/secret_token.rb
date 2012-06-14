5e47b15d7e39ddd52a7fcb7adc0ee811bb562ce2
Andrew Elliott <contact@awegroup.com>
2012-06-13 21:30:24 -0700

    scaffold example initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScaffoldAppExample::Application.config.secret_token = 'f4b00cefa221bf50bacf0be7ec02113d8eeee36f1d2b41b90ef2310dfd03dbf7ef3a23046306117abad65937846e579c6cbf1b715bba26dce3a5682118c566a7'
